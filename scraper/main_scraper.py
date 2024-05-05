f7595e0fc79942f6b3dac1564539232c0dd69ea0
ivanoxx <dafinr13@gmail.com>
2024-05-06 00:56:17 +0700

    edit main scraper

--- scraper/main_scraper.py
+++ scraper/main_scraper.py
@@ -1,8 +1,7 @@
 import jobstreet_scraper, kalibrr_scraper, karir_scraper, linkedin_scraper
 import asyncio
 
-def main():
-    asyncio.run(kalibrr_scraper.scrape())
+def scrape():
+    asyncio.get_event_loop().run_until_complete(kalibrr_scraper.scrape())
 
-if __name__ == '__main__':
-    exit(main())
+scrape()
