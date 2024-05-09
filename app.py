3e3b0ba4238a9c8717225ecb7fcac995e6fb0044
ivanoxx <dafinr13@gmail.com>
2024-05-09 20:43:18 +0700

    create scheduler to scrape

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from flask import Flask, render_template, request
+from flask_apscheduler import APScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 from scraper import main_scraper
-import requests
+import requests, pytz
 
 app = Flask(__name__)
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(main_scraper.scrape, 'cron', hour=1)
-
-@app.before_first_request
-def start_scheduler():
-    scheduler.start()
-
-
-@app.teardown_appcontext
-def stop_scheduler(exception=None):
-    scheduler.shutdown()
+scheduler = BackgroundScheduler(timezone=pytz.timezone('Asia/Jakarta'))
 
 @app.route('/')
 def index():
     return 'halo'
+
+if __name__ == '__main__':
+    scheduler.add_job(main_scraper.scrape, 'cron', hour=21)
+    scheduler.start()
+    app.run()
