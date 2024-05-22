56b3cdeaf8e6ddfb6027781a953b52cefa170893
ivanoxx <dafinr13@gmail.com>
2024-05-22 22:15:43 +0700

    final bismillah

--- app.py
+++ app.py
@@ -8,15 +8,61 @@ scheduler = BackgroundScheduler(timezone=pytz.timezone('Asia/Jakarta'))
 
 @app.route('/')
 def index():
-    response = requests.get('http://localhost:5001/api')
-    
-    if response.status_code == 200:
-        data = response.json()
-        return render_template('page.html', jobs=data)
-    else:
-        return jsonify({'message': 'Failed to fetch data from API'}), 500
+    resp = requests.get('http://localhost:5001/api', params={"offset": 0})
+    params=dict(
+        offset=0,
+        jenis_pekerjaan='',
+        dari_tanggal='',
+        sampai_tanggal='',
+        lokasi='',
+        perusahaan=''
+    )
+    return render_template('page.html', jobs=resp.json(), params=params, scroll=0)
+
+@app.route('/filter')
+def search():
+    jenis_pekerjaan = request.args.get('jenis_pekerjaan')
+    dari_tanggal = request.args.get('dari_tanggal')
+    sampai_tanggal = request.args.get('sampai_tanggal')
+    lokasi = request.args.get('lokasi')
+    perusahaan = request.args.get('perusahaan')
+
+    params=dict(
+        offset=0,
+        jenis_pekerjaan=jenis_pekerjaan,
+        dari_tanggal=dari_tanggal,
+        sampai_tanggal=sampai_tanggal,
+        lokasi=lokasi,
+        perusahaan=perusahaan
+    )
+
+    resp = requests.get('http://localhost:5001/api', params=params)
+    return render_template('page.html', jobs=resp.json(), params=params, scroll=1)
+
+@app.route('/more_jobs')
+def more_jobs():
+    jenis_pekerjaan = request.args.get('jenis_pekerjaan')
+    dari_tanggal = request.args.get('dari_tanggal')
+    sampai_tanggal = request.args.get('sampai_tanggal')
+    lokasi = request.args.get('lokasi')
+    perusahaan = request.args.get('perusahaan')
+    offset = int(request.args.get('offset', 0))
+
+    params=dict(
+        offset=offset,
+        jenis_pekerjaan=jenis_pekerjaan,
+        dari_tanggal=dari_tanggal,
+        sampai_tanggal=sampai_tanggal,
+        lokasi=lokasi,
+        perusahaan=perusahaan
+    )
+
+    print(params)
+
+    additional_jobs =requests.get('http://localhost:5001/api', params=params)
+    return jsonify(additional_jobs.json())
 
 if __name__ == '__main__':
     scheduler.add_job(main_scraper.scrape, 'cron', hour=1)
     scheduler.start()
-    app.run(port=5002, host='0.0.0.0')
+    app.run(debug=True, port=5002, host='0.0.0.0')
