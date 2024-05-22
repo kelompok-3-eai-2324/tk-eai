ebc1bbe993795cdccd921ab6bb5147d58da1daf5
ivanoxx <dafinr13@gmail.com>
2024-05-22 20:04:57 +0700

    change app port

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def index():
     
     if response.status_code == 200:
         data = response.json()
-        return render_template('page.html', data=data)
+        return render_template('page.html', jobs=data)
     else:
         return jsonify({'message': 'Failed to fetch data from API'}), 500
 
 if __name__ == '__main__':
-    app.run()
     scheduler.add_job(main_scraper.scrape, 'cron', hour=1)
     scheduler.start()
+    app.run(port=5002, host='0.0.0.0')
